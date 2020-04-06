5430a6d86d87b46ee5cad0bfc790feb20883aa19
eugene <eu.33.gene@gmail.com>
2020-04-06 12:25:08 +0000

    make title column invalid

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -57,7 +57,8 @@ class TasksController < ApplicationController
 
   # strong parameter
   def task_params
-    params.require(:task).permit(:title, :content, :status)
+#    params.require(:task).permit(:title, :content, :status)
+    params.require(:task).permit(:content, :status)
   end
 
   def correct_user
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
-  validates :title, presence: true, length: { maximum: 255 }
+#  validates :title, presence: true, length: { maximum: 255 }
   validates :content, presence: true, length: { maximum: 255 }
   validates :status, presence: true, length: { maximum: 10 }
 end
